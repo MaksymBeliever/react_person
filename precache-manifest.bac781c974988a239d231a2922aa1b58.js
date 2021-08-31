5293305dc6bc2ef46681e31564fd766f92017b79
Maksym <wolfsangel009@gmail.com>
2021-08-31 23:41:17 +0300

    Updates

---
+++ precache-manifest.bac781c974988a239d231a2922aa1b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5739886e725cc397fc81244093eae29c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a2f37a180eb685448a6",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2f37a180eb685448a6",
+    "url": "./static/js/main.b907bff1.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
